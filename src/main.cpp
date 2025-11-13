a655c09c12765def356a2c448f93d639a4b27ad2
antfoti <a.g.foti1@gmail.com>
2025-11-12 22:08:43 -0600

    Pinout is fully fixed

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
 //Pin out
+//these are right
 #define R1_PIN 25
 #define G1_PIN 26
 #define B1_PIN 27
@@ -9,15 +10,16 @@
 #define G2_PIN 12
 #define B2_PIN 13
 
-#define A_PIN 23
-#define B_PIN 19
-#define C_PIN 5
-#define D_PIN 17
-#define E_PIN 18
+#define A_PIN 23  //yes
+#define B_PIN 19 //yes
+#define C_PIN 5 //yes
+#define D_PIN 18 //yes
+#define E_PIN 17 //yes
 
-#define LAT_PIN 4
-#define OE_PIN 15
-#define CLK_PIN 16
+//issue with these pins
+#define LAT_PIN 4 //moved
+#define OE_PIN 15//moved
+#define CLK_PIN 16 //moved
 
 #define PANEL_RES_X 64
 #define PANEL_RES_Y 64
@@ -25,13 +27,9 @@
 
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
-
-
-
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
+
+  Serial.begin(115200);
   delay(1000);
   
   //init pin connections between esp32 and the LED board
@@ -45,36 +43,31 @@ void setup() {
   //set I2S clock speed
   mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_10M;
 
-
-  //mxconfig.scan = HUB75_I2S_CFG::SCAN_32;
-
-  //printf("Hello World");
-  //Scan config:: come back later and figure this out
-  // mxconfig.scan = HUB75_I2S_CFG::SCAN_16;  // <-- Your panel is 1/16 scan
+  mxconfig.mx_height= 64;
+  mxconfig.mx_width = 64;
   mxconfig.driver = HUB75_I2S_CFG::FM6124;
+
+  mxconfig.clkphase = false;
   //Display Setup
   dma_display = new MatrixPanel_I2S_DMA(mxconfig);
+  
 
-  dma_display->setBrightness8(25); 
-  uint16_t myBLACK = dma_display->color565(0, 0, 0);
+  
+uint16_t myBLACK = dma_display->color565(0, 0, 0);
 uint16_t myWHITE = dma_display->color565(255, 255, 255);
 uint16_t myRED = dma_display->color565(255, 0, 0);
 uint16_t myGREEN = dma_display->color565(0, 255, 0);
 uint16_t myBLUE = dma_display->color565(0, 0, 255);
+uint16_t MAROON = dma_display->color565(128, 0 ,0);
   dma_display->begin();
   //0-255
     //RGB values for certain color we want
-
+  dma_display->setBrightness8(25); 
   dma_display->clearScreen();
 
-  dma_display->fillScreen(myWHITE);
+  dma_display->fillScreen(MAROON);
   delay(1000);
-  dma_display->fillScreen(myRED);
-  delay(1000);
-  dma_display->fillScreen(myGREEN);
-  delay(1000);
-  dma_display->fillScreen(myBLUE);
-  delay(1000);
+  
 
   //dma_display->drawBitmap();
 
@@ -84,12 +77,31 @@ uint16_t myBLUE = dma_display->color565(0, 0, 255);
 
 void loop() {
   
+  uint16_t myBLACK = dma_display->color565(0, 0, 0);
+uint16_t myWHITE = dma_display->color565(255, 255, 255);
+uint16_t myRED = dma_display->color565(255, 0, 0);
+uint16_t myGREEN = dma_display->color565(0, 255, 0);
+uint16_t myBLUE = dma_display->color565(0, 0, 255);
+  
+  dma_display->clearScreen();
+  
+  // dma_display->drawLine(0, 0, 63, 63, dma_display->color565(0, 255, 255));
+  // delay(2000);
+  // dma_display->clearScreen();
+  // dma_display->drawPixel(64,64, myGREEN);
+  // delay(2000);
 
+  dma_display->fillScreen(0);
+  dma_display->drawLine(0, 0, 63, 63, dma_display->color565(0, 255, 255));
+  dma_display->drawCircle(32, 32, 15, dma_display->color565(255, 0, 255));
+  dma_display->drawRect(5, 5, 54, 54, dma_display->color565(255, 255, 0));
+  delay(2000);
 
+  
   
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int draw(int x, int y) {
   return x + y;
 }
