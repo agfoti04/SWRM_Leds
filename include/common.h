acd5e29c3eb8a90c82ec3e7eef6da9787a08eb8f
antfoti <a.g.foti1@gmail.com>
2026-02-03 23:05:45 -0600

    Updated pin out

--- include/common.h
+++ include/common.h
@@ -3,6 +3,7 @@
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
 
+
 extern MatrixPanel_I2S_DMA *dma_display;
 
 
---
+++ include/pinout.h
@@ -0,0 +1,17 @@
+#define R1_Pin 11
+#define G1_pin 13
+#define B1_Pin 15
+#define R2_Pin 17
+#define G2_Pin 19
+#define B2_Pin 21
+
+#define A_Pin 25
+#define B_Pin 27
+#define C_Pin 29
+#define D_Pin 31
+#define E_Pin 23
+
+#define CLK_Pin 33
+#define LAT_Pin 35
+#define OE_Pin 37
+
